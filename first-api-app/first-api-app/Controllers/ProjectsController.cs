30abc07b03e5086b995dd5fa48b2673db8584201
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-05 10:13:09 +0530

    api basics

---
+++ first-api-app/first-api-app/Controllers/ProjectsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_api_app.Controllers
+{
+	[Route("api/projects")]
+	public class ProjectsController : ControllerBase
+	{
+		public ProjectsController()
+		{
+			
+		}
+
+		[HttpGet()]
+		public ActionResult GetProjects()
+        {
+			
+			var projects = new List<Object>
+			{
+				new {id = 1, name = "Expense Manager"},
+				new {id = 2, name = "Bug Tracker"}
+			};
+
+			return new JsonResult(projects);
+        }
+
+		[HttpGet("{id}")]
+		public ActionResult GetProjectById(int id)
+		{
+			var project = new {
+				id = id,
+				name = "Project - " + id
+			};
+			return new JsonResult(project);
+        }
+
+		[HttpPost()]
+		public string CreateProject()
+        {
+			return "A new project is created!";
+        }
+	}
+}
+
---
+++ first-api-app/first-api-app/Program.cs
@@ -0,0 +1,43 @@
+﻿//Top Level Statement
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+/*
+app.Run(async (context) =>
+{
+    //analyze the request
+    //extract the data from the payload
+    //deserialize it
+    //convert the data into a .NET objecct
+    //process the data
+    //get the result
+    //seraialize the result
+    //send the response
+    await context.Response.WriteAsync("Hello World!");
+});
+*/
+
+app.Run();
+
